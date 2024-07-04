199ac0c260a970e3b25929b5fcb3904e06372111
JagadeshMaddileti <jagadesh.maddileti@zemosolabs.com>
2024-07-04 21:26:42 +0530

    Update assignment-function-2.js

--- assignment-function-2.js
+++ assignment-function-2.js
@@ -1,2 +1,2 @@
 const concatFirstIndex=(firstName,lastName) => firstName.at(0)+lastName.at(0);  //arrow function
-console.log(concatFirstIndex("Roger","Waters"));git
+console.log(concatFirstIndex("Roger","Waters"));
