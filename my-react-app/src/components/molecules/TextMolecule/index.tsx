4fb19104469adbf2021215a65a32914e8e236a87
JagadeshMaddileti <jagadeshmaddileti@zemosolabs.com>
2024-07-15 20:00:26 +0530

    Feat change component name.

--- my-react-app/src/components/molecules/TextMolecule/index.tsx
+++ my-react-app/src/components/molecules/TextMolecule/index.tsx
@@ -1,13 +1,13 @@
 import { Stack } from "@mui/material";
-import CustomTypography from "../../atoms/Typography";
+import Typography from "../../atoms/Typography";
 import "./index.css";
 
 const Texts=()=>{
     return(
         <Stack direction="column">
-            <CustomTypography variant="h5" label ="User Experience Designer"/>
-            <CustomTypography variant="caption" label ="Myntra" className="myntra"/>
-            <CustomTypography variant="caption" label ="Hitech city, Hyderabad - 500072" className="address"/>
+            <Typography variant="h5" label ="User Experience Designer"/>
+            <Typography variant="caption" label ="Myntra" className="myntra"/>
+            <Typography variant="caption" label ="Hitech city, Hyderabad - 500072" className="address"/>
         </Stack>
     );
 }
